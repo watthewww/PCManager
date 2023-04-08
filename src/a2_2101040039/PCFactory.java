430ca12c6cf73cf463048bf47baf1f3ebb80bcc1
Wat <chiptp1912@gmail.com>
2023-04-08 17:20:37 +0700

    test 1

--- src/a2_2101040039/PCFactory.java
+++ src/a2_2101040039/PCFactory.java
@@ -1,7 +1,11 @@
 package a2_2101040039;
 
 public class PCFactory {
+    private static PCFactory factory = new PCFactory();
     private PCFactory() {
+    }
+    public static PCFactory getFactory() {
+        return factory;
     }
     public static PC createObjects(String model, int year, String manufacturer, Set<String> comps) {
             return new PC(model, year, manufacturer, comps);
--- src/a2_2101040039/PCReport.java
+++ src/a2_2101040039/PCReport.java
@@ -1,15 +1,18 @@
 package a2_2101040039;
 
-import java.util.Arrays;
-
 public class PCReport {
-    public static String displayReport(PC[] objs) {
+    public static void displayReport(PC[] objs) {
         StringBuilder line = new StringBuilder();
-        StringBuilder sb = new StringBuilder();
         line.append("-".repeat(99));
         String header = String.format("%56s", "PCPROG REPORT");
-        String pc = Arrays.toString(objs);
-        String report = String.valueOf(sb.append(line).append(header).append(line));
-    return report;
+        System.out.println(line);
+        System.out.println(header);
+        System.out.println(line);
+        for (int i = 0; i < objs.length; i++) {
+            PC pc = objs[i];
+            System.out.format("%3s %20s %6s %15s %-55s", i + 1, pc.getModel(), pc.getYear(), pc.getManufacturer(),
+                    pc.getComps());
+            System.out.println();
+        }
     }
 }
