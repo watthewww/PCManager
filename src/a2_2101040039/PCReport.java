a0f748b18c17a46f9dbd6b0038ac17cd2fb11eb5
Wat <chiptp1912@gmail.com>
2023-04-08 18:25:09 +0700

    test 1

--- src/a2_2101040039/PCReport.java
+++ src/a2_2101040039/PCReport.java
@@ -1,18 +1,27 @@
 package a2_2101040039;
 
 public class PCReport {
-    public static void displayReport(PC[] objs) {
+    PCReport() {
+    }
+
+    public static String displayReport(PC[] objs) {
         StringBuilder line = new StringBuilder();
+        StringBuilder info = new StringBuilder();
         line.append("-".repeat(99));
         String header = String.format("%56s", "PCPROG REPORT");
-        System.out.println(line);
-        System.out.println(header);
-        System.out.println(line);
+        info.append(line);
+        info.append(System.getProperty("line.separator"));
+        info.append(header);
+        info.append(System.getProperty("line.separator"));
+        info.append(line);
+        info.append(System.getProperty("line.separator"));
+
         for (int i = 0; i < objs.length; i++) {
             PC pc = objs[i];
-            System.out.format("%3s %20s %6s %15s %-55s", i + 1, pc.getModel(), pc.getYear(), pc.getManufacturer(),
-                    pc.getComps());
-            System.out.println();
+            String temp = String.format("%3s %20s %6s %15s %-55s %n", i + 1, pc.getModel(), pc.getYear(), pc.getManufacturer(), pc.getComps());
+            info.append(temp);
+            //System.out.format("%3s %20s %6s %15s %-55s", i + 1, pc.getModel(), pc.getYear(), pc.getManufacturer(), pc.getComps());
         }
+        return info.toString();
     }
 }
